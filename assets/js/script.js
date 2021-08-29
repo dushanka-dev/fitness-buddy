2ac5b4a60ea1d31c8845ca19c3fcb888fd23a136
Dushanka Perera <dushankadev@gmail.com>
2021-08-29 14:27:45 +0000

    Fix submit btn bug by adjusting event listener code and adding new event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let addBtn = document.getElementById('exercise-btn');
+// document.addEventListener('DOMContentLoaded', function() {
+//     let addBtn = document.getElementById('exercise-btn');
 
-    addBtn.addEventListener('submit', function(event) {
-        if (this.getAttribute('id') === 'exercise-btn') {
-            addExercises()
-        }
-    })
+//     addBtn.addEventListener('submit', function(event) {
+//         if (this.getAttribute('id') === 'exercise-btn') {
+//             addExercises()
+//         }
+//     })
 
-})
+// })
 
 // Add/Remove Exercises
 
@@ -19,12 +19,16 @@ function addExercises(event) {
     let exerciseInputs = document.getElementById('exercise-input').value;
 
     let newExercise = document.createElement('li');
+    let removeBtn = document.createElement('button')
 
     newExercise.className = 'exercise-items';
     newExercise.appendChild(document.createTextNode(exerciseInputs));
     newList.appendChild(newExercise)
 
 }
+
+const addBtn = document.getElementById('exercise-btn');
+addBtn.addEventListener('click', addExercises);
 
 
 // Remove Exercises from List
