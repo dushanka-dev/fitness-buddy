2e6bc131413d5f274c87519688490e304f7eaa9d
Dushanka Perera <dushankadev@gmail.com>
2021-08-30 12:21:34 +0000

    Add code to alert user if input field empty

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,21 +3,17 @@ document.addEventListener("DOMContentLoaded", function() {
     // Exercise List Event Listener
     const exerciseForms = document.getElementById('exercise-form');
     exerciseForms.addEventListener('submit', addExercises);
-   
+
 });
 
 function addExercises(event) {
 
         event.preventDefault();
     
-        const newList = document.getElementById('exercise-group')
-        const exerciseInputs = document.getElementById('exercise-input').value;
-        
-        // Clear field after input
-        document.getElementById('exercise-input').value = '';
-    
-        const newExercise = document.createElement('li');
-        const removeBtn = document.createElement('button')
+        let newList = document.getElementById('exercise-group')
+        let exerciseInputs = document.getElementById('exercise-input').value;
+        let newExercise = document.createElement('li');
+        let removeBtn = document.createElement('button')
     
         newExercise.classList.add('exercise-items');
         newExercise.innerText = exerciseInputs;
@@ -28,6 +24,16 @@ function addExercises(event) {
         removeBtn.classList.add('delete-btn');
         removeBtn.innerText = 'X';
         newExercise.appendChild(removeBtn);
+
+        // Clear field after input
+        document.getElementById('exercise-input').value = '';
+
+        // Alert User
+
+        if (exerciseInputs === '') {
+            confirm("Please Add Exercise!");
+            newExercise.remove('li')
+        }
     
     };
 
